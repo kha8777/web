04bd5d77f7665ae670edb438ae8d9c953e208f4d
kha8777 <khatran17042002@gmail.com>
2022-04-11 14:44:26 +0700

    add search-history, update css

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
-    $("#search-box").hover(function() {
-        $("#search-box__input").css({"width": "320px", "padding": "0 12px"});
-        $("#search-box__input").addClass('search-box__expanded');
-    });
-});
-
-document.addEventListener('click', function(e) {
-    var target = e.target;
-    var input = document.getElementById('search-box__input');
-    if (target.id != 'search-box__input') {
-        input.style.width = '0';
-        input.style.padding = '0';
-    }
-}, false);
+    $('.search-history__item-link').click(function() {
+        
+    })
+})
