77542d2379bcc32cd3abbc68d3e6f1ab0c467627
kha8777 <khatran17042002@gmail.com>
2022-04-16 21:20:04 +0700

    update form dang nhap va form dang ky

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,50 @@
 $(document).ready(function() {
+    
+    // khi click vào đăng ký thì sẽ hiện form đăng ký ra
     $('.signup').click(function() {
         $('.signup-form-wrap').css("display", "block");
     })
 
+    // khi click vào đăng nhập thì sẽ hiện form đăng nhập ra
+    $('.login').click(function() {
+        $('.login-form-wrap').css("display", "block");
+    })
+
+    // khi click nút X ở form đăng nhập hoặc đăng ký thì đóng form lại
     $('.close').click(function() {
         $('.signup-form-wrap').css("display", "none");
         $('.login-form-wrap').css("display", "none");
     })
+
+    // khi click nút hủy thì đóng form lại
+    $('.btn-cancel').click(function() {
+        $('.signup-form-wrap').css("display", "none");
+        $('.login-form-wrap').css("display", "none");
+    })
     
+    // cái này định làm khi click vào item ở phần lịch sử tìm kiếm
+    // thì không bị mất input (bug)
     $('.search-history__item-link').click(function() {
         $('.search-box__input').focus();
         $('.search-box__input').css("width", "300px");
         $('.search-box__input').css("padding", "0 15px 0 12px;");
     })
+    
+    // chuyển sang đăng ký khi click vào "Chưa có tài khoản?"
+    $('.switch-signup-form').click(function() {
+        $('.login-form-wrap').css("display", "none");
+        $('.signup-form-wrap').css("display", "block");
+    })
+    
+    // chuyển sang đăng nhập khi click vào "Đã có tài khoản?"
+    $('.switch-login-form').click(function() {
+        $('.signup-form-wrap').css("display", "none");
+        $('.login-form-wrap').css("display", "block");
+    })
 })
 
+// kiểm tra xem người dùng nhập đúng định dạng tài khoản khi đăng ký không
+// tài khoản chứa ít nhất 3 ký tự, bao gồm chữ hoa, chữ thường và số và không chứa dấu cách
 function checkUser() {
     var userInput = document.getElementById('user-input').value;
     var userError = document.getElementById('user-error');
@@ -30,6 +60,8 @@ function checkUser() {
     return true;
 }
 
+// kiểm tra xem người dùng nhập đúng định dạng mật khẩu khi đăng ký không
+// tài khoản chứa ít nhất 6 ký tự, chứa ít nhất một chữ hoa, ít nhất một chữ thường, ít nhất một chữ số, cho phép ký tự và không chứa dấu cách
 function checkPwd() {
     var pwdInput = document.getElementById('pwd-input').value;
     var pwdError = document.getElementById('pwd-error');
@@ -45,6 +77,7 @@ function checkPwd() {
     return true;
 }
 
+// kiểm tra xem người dùng nhập lại mật khẩu có chính xác không
 function checkRepwd() {
     var pwdInput = document.getElementById('pwd-input').value;
     var repwdInput = document.getElementById('repwd-input').value;
@@ -57,6 +90,7 @@ function checkRepwd() {
     return true;
 }
 
+// kiểm tra tất cả định dạng nhập vào form đăng ký
 function validInfo() {
     if (checkUser() && checkPwd() && checkRepwd()) {
         return true;
